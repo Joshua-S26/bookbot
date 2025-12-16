5938e61fbfcc6f4d83672ca86b486b5e5bb31b23
Joshua-S26 <jshort26@sbcglobal.net>
2025-12-16 11:37:33 -0800

    Bookbot core functions done

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from stats import get_number_words
+from stats import get_number_words, get_chars_dict, sorted_chars
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_number_words(text)
-    print(f"Found {num_words} total words")
+    num_char = get_chars_dict(text)
+    sorted_list = sorted_chars(num_char)
 
+    # print(f"Found {num_words} total words")
+    # print("Character count:", num_char)
+    print(f'''
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found 75767 total words
+--------- Character Count -------
+          ''')
+    for item in sorted_list:
+        ch = item["char"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 main()
+
--- stats.py
+++ stats.py
@@ -2,3 +2,23 @@ def get_number_words(text):
     count = 0
     words = text.split()
     return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sort_on(item): 
+    return item["num"]
+
+def sorted_chars(char_dict):
+    list_of_dicts = []
+    for ch, count in char_dict.items():
+        list_of_dicts.append({"char": ch, "num": count})
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
