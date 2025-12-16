c740e992edc9d4136640d804cad8a31ed3f3e730
Joshua-S26 <jshort26@sbcglobal.net>
2025-12-16 10:11:02 -0800

    Counted number of words

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_number_words(text)
+    print(f"Found {num_words} total words")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_number_words(text):
+    count = 0
+    words = text.split()
+    return len(words)
+main()
